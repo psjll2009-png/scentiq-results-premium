2ea0c318c0aa5fbe4dce06186ac41a65fb40774f
psjll2009-png <psjll2009@gmail.com>
2025-10-31 00:27:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function saveBlend() {
+  alert("This would trigger saving functionality for premium users.");
+  // Could be connected to Airtable or API later
+}
+
+function remixBlend() {
+  alert("This would send the blend back through AI for a remix.");
+  // Could connect to GPT/Zapier if needed
+}
